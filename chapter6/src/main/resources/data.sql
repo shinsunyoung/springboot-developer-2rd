e107cf8c882445aa988f96c0db1b747986510af6
shinsunyoung <ajufresh@gmail.com>
2023-11-05 03:28:26 +0900

    docs: add chpater6 code

---
+++ chapter6/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (title, content) VALUES ('제목 1', '내용 1')
+INSERT INTO article (title, content) VALUES ('제목 2', '내용 2')
+INSERT INTO article (title, content) VALUES ('제목 3', '내용 3')
