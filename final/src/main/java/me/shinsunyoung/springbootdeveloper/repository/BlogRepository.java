ebb8b0ecec32a9fad84802c3bb6c89b95b44bd00
shinsunyoung <ajufresh@gmail.com>
2023-11-05 17:23:53 +0900

    docs: delete old code

--- final/src/main/java/me/shinsunyoung/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package me.shinsunyoung.springbootdeveloper.repository;
-
-import me.shinsunyoung.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
-
