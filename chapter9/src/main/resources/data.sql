607da2548a331d449e15f64e407718e02a501f3d
shinsunyoung <ajufresh@gmail.com>
2023-11-05 13:23:22 +0900

    docs: add chpater9 code

---
+++ chapter9/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목 1', '내용 1', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목 2', '내용 2', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목 3', '내용 3', NOW(), NOW())
