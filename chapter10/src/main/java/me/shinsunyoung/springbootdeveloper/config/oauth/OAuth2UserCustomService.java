367267e0dd0bd9e627c87dfa0e47084f104bf6fe
shinsunyoung <ajufresh@gmail.com>
2023-11-05 14:09:50 +0900

    docs: add chpater10 code

---
+++ chapter10/src/main/java/me/shinsunyoung/springbootdeveloper/config/oauth/OAuth2UserCustomService.java
@@ -0,0 +1,43 @@
+package me.shinsunyoung.springbootdeveloper.config.oauth;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.User;
+import me.shinsunyoung.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+public class OAuth2UserCustomService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User user = super.loadUser(userRequest);
+        saveOrUpdate(user);
+
+        return user;
+    }
+
+    private User saveOrUpdate(OAuth2User oAuth2User) {
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        String email = (String) attributes.get("email");
+        String name = (String) attributes.get("name");
+
+        User user = userRepository.findByEmail(email)
+                .map(entity -> entity.update(name))
+                .orElse(User.builder()
+                        .email(email)
+                        .nickname(name)
+                        .build());
+
+        return userRepository.save(user);
+    }
+}
